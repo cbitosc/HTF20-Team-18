0f12b13c49a8b7e07ca4807ea6b477b26395f64d
AlekyaKowta <akowta@gmail.com>
2020-10-31 03:33:04 +0530

    chat box

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ var session = require('express-session');
 const app = express();
 const mongoose = require('mongoose');
 app.set('view engine', 'ejs');
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+
 app.use(express.urlencoded({
     extended: true
 }));
@@ -27,54 +26,119 @@ app.use(upload.array());
 
 const user = require('./models/user');
 
-const rooms = { };
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+  } = require('./utils/users');
 
-app.get('/forum', (req, res) => {
-    res.render('forum.ejs', { rooms: rooms })
-  })
-  
-  app.post('/room', (req, res) => {
-    if (rooms[req.body.room] != null) {
-      return res.redirect('/')
+const server = http.createServer(app);
+const io = socketio(server);
+
+const botName = 'ChatCord Bot';
+
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
     }
-    rooms[req.body.room] = { users: {} }
-    res.redirect(req.body.room)
-    // Send message that new room was created
-    io.emit('room-created', req.body.room)
-  })
+  });
+});
+
+
+
+// const rooms = { };
+
+// app.get('/forum', (req, res) => {
+//     res.render('forum.ejs', { rooms: rooms })
+//   })
   
-  app.get('/:room', (req, res) => {
-    if (rooms[req.params.room] == null) {
-      return res.redirect('/')
-    }
-    res.render('room', { roomName: req.params.room })
-  })
+//   app.post('/room', (req, res) => {
+//     if (rooms[req.body.room] != null) {
+//       return res.redirect('/')
+//     }
+//     rooms[req.body.room] = { users: {} }
+//     res.redirect(req.body.room)
+//     // Send message that new room was created
+//     io.emit('room-created', req.body.room)
+//   })
   
-//server.listen(3000)
+//   app.get('/:room', (req, res) => {
+//     if (rooms[req.params.room] == null) {
+//       return res.redirect('/')
+//     }
+//     res.render('room', { roomName: req.params.room })
+//   })
   
-  io.on('connection', socket => {
-    socket.on('new-user', (room, name) => {
-      socket.join(room)
-      rooms[room].users[socket.id] = name
-      socket.to(room).broadcast.emit('user-connected', name)
-    })
-    socket.on('send-chat-message', (room, message) => {
-      socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
-    })
-    socket.on('disconnect', () => {
-      getUserRooms(socket).forEach(room => {
-        socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
-        delete rooms[room].users[socket.id]
-      })
-    })
-  })
+// //server.listen(3000)
   
-  function getUserRooms(socket) {
-    return Object.entries(rooms).reduce((names, [name, room]) => {
-      if (room.users[socket.id] != null) names.push(name)
-      return names
-    }, [])
-  }
+//   io.on('connection', socket => {
+//     socket.on('new-user', (room, name) => {
+//       socket.join(room)
+//       rooms[room].users[socket.id] = name
+//       socket.to(room).broadcast.emit('user-connected', name)
+//     })
+//     socket.on('send-chat-message', (room, message) => {
+//       socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+//     })
+//     socket.on('disconnect', () => {
+//       getUserRooms(socket).forEach(room => {
+//         socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+//         delete rooms[room].users[socket.id]
+//       })
+//     })
+//   })
+  
+//   function getUserRooms(socket) {
+//     return Object.entries(rooms).reduce((names, [name, room]) => {
+//       if (room.users[socket.id] != null) names.push(name)
+//       return names
+//     }, [])
+//   }
 
 
 mongoose.connect(process.env.DB_CONNECT, {
