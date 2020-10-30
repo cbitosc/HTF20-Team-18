a93aa705270ad559cb13f79b8290a86b9beaf3ac
AlekyaKowta <akowta@gmail.com>
2020-10-31 02:49:47 +0530

    chat room

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ var upload = multer();
 
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+
 const app = express();
 const mongoose = require('mongoose');
 app.set('view engine', 'ejs');
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 app.use(express.urlencoded({
     extended: true
 }));
@@ -22,7 +25,56 @@ app.use(cookieParser());
 app.use(session({secret: "Shh, its a secret!"}));
 app.use(upload.array());
 
-const user = require('./models/user')
+const user = require('./models/user');
+
+const rooms = { };
+
+app.get('/forum', (req, res) => {
+    res.render('forum.ejs', { rooms: rooms })
+  })
+  
+  app.post('/room', (req, res) => {
+    if (rooms[req.body.room] != null) {
+      return res.redirect('/')
+    }
+    rooms[req.body.room] = { users: {} }
+    res.redirect(req.body.room)
+    // Send message that new room was created
+    io.emit('room-created', req.body.room)
+  })
+  
+  app.get('/:room', (req, res) => {
+    if (rooms[req.params.room] == null) {
+      return res.redirect('/')
+    }
+    res.render('room', { roomName: req.params.room })
+  })
+  
+//server.listen(3000)
+  
+  io.on('connection', socket => {
+    socket.on('new-user', (room, name) => {
+      socket.join(room)
+      rooms[room].users[socket.id] = name
+      socket.to(room).broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', (room, message) => {
+      socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+      getUserRooms(socket).forEach(room => {
+        socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+        delete rooms[room].users[socket.id]
+      })
+    })
+  })
+  
+  function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+      if (room.users[socket.id] != null) names.push(name)
+      return names
+    }, [])
+  }
 
 
 mongoose.connect(process.env.DB_CONNECT, {
